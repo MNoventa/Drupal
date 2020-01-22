af28e537ca53fe5eb434295afe1088aca096f36b
Mario Garcia <mg@drauta.com>
2020-01-22 15:47:07 +0100

    test atom 5

--- acceder a propiedad protegida en paragraph.php
+++ acceder a propiedad protegida en paragraph.php
@@ -3,7 +3,7 @@
 	//Acceder a campo protected en un paragraph en Drupal 8
 	// test
 	trader_cdlgapsidesidewhite
- 
+   
 		{{ kint(content.field_catalogo.0['#paragraph'].field_imagen.entity.uri.getValue.0.value) }}
 
 ?>
